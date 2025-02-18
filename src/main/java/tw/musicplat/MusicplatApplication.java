301fc6a00b6b0fa6121a288d407c966d4a3e2d3d
andy <a0916720349@gmail.com>
2025-02-18 21:41:59 +0800

    hello

---
+++ src/main/java/tw/musicplat/MusicplatApplication.java
@@ -0,0 +1,13 @@
+package tw.musicplat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicplatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicplatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tw/musicplat/MusicplatApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.musicplat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicplatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
