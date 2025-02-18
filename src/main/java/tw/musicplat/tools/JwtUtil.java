ce7f4982ce3d5b9a2732c18d83971354c29e02e8
andy <a0916720349@gmail.com>
2025-02-18 23:02:34 +0800

    hello

--- src/main/java/tw/musicplat/tools/JwtUtil.java
+++ src/main/java/tw/musicplat/tools/JwtUtil.java
@@ -1,5 +1,34 @@
 package tw.musicplat.tools;
 
-public class JwtUtil {
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import tw.musicplat.config.MyUserDetail;
 
+import java.util.*;
+
+public class JwtUtil {
+    @Value("${jwtToken.secret}")
+    private static String SECRET;
+    private static final int EXPIRE_TIME = 60 * 60 ;
+
+    // 產生token
+    public static String generateToken(Authentication authentication) {
+        MyUserDetail principal = (MyUserDetail)authentication.getPrincipal();
+        // payload
+        System.out.println("authentication.getAuthorities():"+authentication.getAuthorities());
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("username", principal.getUsername());
+        map.put("roles", principal.getAuthorities());
+
+        return Jwts.builder() // 使用流化的建構器
+                .claims(map)
+                .subject(principal.getId()+"") // 設定主承載內容,通常是唯一識別id
+                .issuedAt(new Date()) // 發行時間
+                .expiration(new Date(System.currentTimeMillis() + EXPIRE_TIME * 1000L)) // 到期時間
+                .signWith(Keys.hmacShaKeyFor(SECRET.getBytes())) // 密鑰簽名
+                .compact(); // 產生出JWT token
+    }
 }
---
+++ src/test/java/tw/musicplat/TestZzz.java
@@ -0,0 +1,9 @@
+package tw.musicplat;
+
+import org.junit.jupiter.api.Test;
+
+public class TestZzz {
+    @Test
+    public void test() {
+    }
+}
